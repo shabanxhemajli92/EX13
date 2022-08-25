3c93151612cb3c6ec5546f50b9bdce7762747a5b
Shaban <shabanxhemajli@live.com>
2022-08-25 14:21:02 +0200

    first commit

---
+++ EX13.py
@@ -0,0 +1,7 @@
+upper_phrase=input("Type the phrase you want uppercased :")
+upper_phrase=upper_phrase.upper()
+lower_phrase=input("TypType the phrase you want lowerrcased :")
+lower_phrase=lower_phrase.lower()
+
+print(upper_phrase)
+print(lower_phrase)
